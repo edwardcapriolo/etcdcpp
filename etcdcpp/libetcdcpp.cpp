281e7d707874dc0f974f5f10a0f36939bf5a617e
Joseph Price <joseph.price@huffingtonpost.com>
2014-09-09 18:01:03 -0400

    Separate header and implementation

--- etcdcpp/libetcdcpp.cpp
+++ etcdcpp/libetcdcpp.cpp
@@ -1,7 +1,4 @@
 
-#ifndef LIBETCDCPP_cxx_
-#define LIBETCDCPP_cxx_
-
 #include <curl/curl.h>
 #include <iostream>
 #include <vector>
@@ -10,24 +7,14 @@
 #include "rapidjson/writer.h"
 #include "rapidjson/stringbuffer.h"
 #include <memory>
+#include "libetcdcpp.h"
 
 using namespace std;
 using namespace rapidjson;
-
-namespace etcdcpp {
-
-class etcd_host {
-public:
-  etcd_host(string host, short port){
-    this -> host = host;
-    this -> port = port;
-  }
-  string host;
-  unsigned short port;
-};
+using namespace etcdcpp;
 
 /* curl uses a callback to read urls. It passes the result buffer reference as an argument */
-int writer(char *data, size_t size, size_t nmemb, string *buffer){
+int etcdcpp::writer(char *data, size_t size, size_t nmemb, string *buffer){
   int result = 0;
   if(buffer != NULL) {
     buffer -> append(data, size * nmemb);
@@ -36,114 +23,97 @@ int writer(char *data, size_t size, size_t nmemb, string *buffer){
   return result;
 } 
 
-class etcd_session {
-
-public:
-  etcd_session(vector<etcd_host> server_list) {
-    if (server_list.size() == 0) {
-      throw "startup failed";
-    }
-    this->server_list = server_list;
-    curl_global_init(CURL_GLOBAL_ALL);
+etcd_session::etcd_session(vector<etcd_host> server_list) {
+  if (server_list.size() == 0) {
+    throw "startup failed";
   }
+  this->server_list = server_list;
+  curl_global_init(CURL_GLOBAL_ALL);
+}
 
-  etcd_session(etcd_host host) {
-    vector<etcd_host> hosts;
-    hosts.push_back(host);
-    this->server_list = hosts;
-    curl_global_init(CURL_GLOBAL_ALL);
-  }
+etcd_session::etcd_session(etcd_host host) {
+  vector<etcd_host> hosts;
+  hosts.push_back(host);
+  this->server_list = hosts;
+  curl_global_init(CURL_GLOBAL_ALL);
+}
 
-  /**
-  * User must free returned Document *
-  */
-  std::unique_ptr<Document> get(string key){
-    CURL *curl;
-    CURLcode res;
-    curl = curl_easy_init();
-    string buffer;	
-    if(curl) {
-      for (int i=0 ; i< server_list.size() ; i++){
-        curl_easy_setopt(curl, CURLOPT_URL, ("http://" + server_list[0].host + ":" + "4001" + "/v2/keys" + key + "?sorted=true&recursive=true").c_str());
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &writer);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
-        res = curl_easy_perform(curl);
-        if(res != CURLE_OK) {
-          fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-          throw res;
-        }
-        curl_easy_cleanup(curl);
-        std::unique_ptr<Document> d (new Document());
-        d->Parse(buffer.c_str());
-        //{"errorCode":100,"message":"Key not found","cause":"/message","index":14}
-        return d;
+std::unique_ptr<Document> etcd_session::get(string key) {
+  CURL *curl;
+  CURLcode res;
+  curl = curl_easy_init();
+  string buffer;	
+  if (curl) {
+    for (int i=0 ; i< server_list.size() ; i++){
+      curl_easy_setopt(curl, CURLOPT_URL, ("http://" + server_list[0].get_host() + ":" + "4001" + "/v2/keys" + key + "?sorted=true&recursive=true").c_str());
+      curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &writer);
+      curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
+      res = curl_easy_perform(curl);
+      if(res != CURLE_OK) {
+        fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+        throw res;
       }
+      curl_easy_cleanup(curl);
+
+      Document *d = new Document;
+      d->Parse(buffer.c_str());
+      return std::unique_ptr<Document>(std::move(d));
     }
-    throw "curl issue";
   }
+  throw "curl issue";
+}
   
-  /**
-  * User must free returned Document *
-  */
-  std::unique_ptr<Document> set(string key, string value, int ttl) {
-    CURL *curl;
-    CURLcode res;
-    curl = curl_easy_init();
-    string buffer = "value=" + value + "&ttl=" + "60";
-    string result;
-    if(curl) {
-      for (int i=0 ; i< server_list.size() ; i++){
-        curl_easy_setopt(curl, CURLOPT_URL, ("http://" + server_list[0].host + ":" + "4001" + "/v2/keys" + key).c_str());
-        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, buffer.c_str());
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &writer);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &result);
-        res = curl_easy_perform(curl);
-        if(res != CURLE_OK){
-          fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-          throw res;
-        }
-        curl_easy_cleanup(curl);
-	std::unique_ptr<Document> d(new Document);
-        d->Parse(result.c_str());
-        return d;
+std::unique_ptr<Document> etcd_session::set(string key, string value, int ttl) {
+  CURL *curl;
+  CURLcode res;
+  curl = curl_easy_init();
+  string buffer = "value=" + value + "&ttl=" + "60";
+  string result;
+  if(curl) {
+    for (int i=0 ; i< server_list.size() ; i++){
+      curl_easy_setopt(curl, CURLOPT_URL, ("http://" + server_list[0].get_host() + ":" + "4001" + "/v2/keys" + key).c_str());
+      curl_easy_setopt(curl, CURLOPT_POSTFIELDS, buffer.c_str());
+      curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &writer);
+      curl_easy_setopt(curl, CURLOPT_WRITEDATA, &result);
+      res = curl_easy_perform(curl);
+      if(res != CURLE_OK){
+        fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+        throw res;
       }
-    } 
-    throw "curl issue";
-  }
-
-  /**
-  * User must free returned Document *
-  */
-  std::unique_ptr<Document> set(string key, string value) {
-    CURL *curl;
-    CURLcode res;
-    curl = curl_easy_init();
-    string buffer = "value=" + value;
-    string result;
-    if(curl) {
-      for (int i=0 ; i< server_list.size() ; i++){
-        curl_easy_setopt(curl, CURLOPT_URL, ("http://" + server_list[0].host + ":" + "4001" + "/v2/keys" + key).c_str());
-        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, buffer.c_str());
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &writer);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &result);
-        res = curl_easy_perform(curl);
-        if(res != CURLE_OK){
-          fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-          throw res;
-        }
-        curl_easy_cleanup(curl);
-        std::unique_ptr<Document> d ( new Document());
-        d->Parse(result.c_str());
-        return d;
-      }
+      curl_easy_cleanup(curl);
+      
+      Document *d = new Document;
+      d->Parse(result.c_str());
+      return std::unique_ptr<Document>(std::move(d));
     }
-    throw "curl issue";
+  } 
+  throw "curl issue";
+}
+
+std::unique_ptr<Document> etcd_session::set(string key, string value) {
+  CURL *curl;
+  CURLcode res;
+  curl = curl_easy_init();
+  string buffer = "value=" + value;
+  string result;
+  if(curl) {
+    for (int i=0 ; i< server_list.size() ; i++){
+      curl_easy_setopt(curl, CURLOPT_URL, ("http://" + server_list[0].get_host() + ":" + "4001" + "/v2/keys" + key).c_str());
+      curl_easy_setopt(curl, CURLOPT_POSTFIELDS, buffer.c_str());
+      curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &writer);
+      curl_easy_setopt(curl, CURLOPT_WRITEDATA, &result);
+      res = curl_easy_perform(curl);
+      if(res != CURLE_OK){
+        fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+        throw res;
+      }
+      curl_easy_cleanup(curl);
+
+      Document *d = new Document;
+      d->Parse(result.c_str());
+      return std::unique_ptr<Document>(std::move(d));
+    }
   }
+  throw "curl issue";
+}
 
-private:
-  vector<etcd_host> server_list;
-};
-
-} //end namespace
-
-#endif
---
+++ etcdcpp/libetcdcpp.h
@@ -0,0 +1,57 @@
+
+#ifndef LIBETCDCPP_cxx_
+#define LIBETCDCPP_cxx_
+
+#include <vector>
+#include <string.h>
+#include "rapidjson/document.h"
+
+using namespace std;
+using namespace rapidjson;
+
+namespace etcdcpp {
+
+/* curl uses a callback to read urls. It passes the result buffer reference as an argument */
+int writer(char *data, size_t size, size_t nmemb, string *buffer);
+
+class etcd_host {
+  public:
+    etcd_host(string host, short port): host(host), port(port) {};
+
+    string get_host() { return host; }
+
+    unsigned short get_port() { return port; }
+
+  private: 
+    string host;
+    unsigned short port;
+};
+
+class etcd_session {
+  public:
+    etcd_session(vector<etcd_host> server_list);
+
+    etcd_session(etcd_host host);
+
+    /**
+    * User must free returned Document *
+    */
+    std::unique_ptr<Document> get(string key);
+    
+    /**
+    * User must free returned Document *
+    */
+    std::unique_ptr<Document> set(string key, string value, int ttl);
+
+    /**
+    * User must free returned Document *
+    */
+    std::unique_ptr<Document> set(string key, string value);
+
+    private:
+      vector<etcd_host> server_list;
+};
+
+} //end namespace
+
+#endif
