56808cf4b62ec2c54e0ac3152859d11f9f3f6967
Joseph Price <joseph.price@huffingtonpost.com>
2014-09-10 18:44:26 -0400

    Add install target to cmake. Rename files

--- etcdcpp/etcdcpp.cpp
+++ etcdcpp/etcdcpp.cpp
@@ -8,7 +8,7 @@
 #include "rapidjson/writer.h"
 #include "rapidjson/stringbuffer.h"
 #include <memory>
-#include "libetcdcpp.h"
+#include "etcdcpp.h"
 
 using namespace std;
 using namespace rapidjson;
